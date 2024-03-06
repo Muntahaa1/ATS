6a018fd82cf376b53214b1b66775f9d86f2f52f6
Muntahaa Khan <103258353+Pasha-007@users.noreply.github.com>
2024-03-07 02:07:36 +0600

    Smooth scroll feature

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll to the target element
+document.querySelectorAll('.options-container a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
